0e7f906801b0cc41acaa7ceaee8e01e3eaddd6e6
sehyun <sehyun.lee217@gmail.com>
2023-08-21 19:10:52 -0400

    Changes to index.html and Aboutcomponent

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,9 @@ export default function About() {
     return (
         <>
             <header><h1>About</h1></header>
-            <main></main>
+            <main>
+                <div>I'm a second year engineering student at University of Toronto</div>
+            </main>
             <footer></footer>
         </>
     );
